a33f33eda602b3e2a355c6cf016409dc53d983f3
Misumi Rize <r@ayase-e.li>
2014-07-29 22:27:44 +0900

    browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
 var gulp = require("gulp"),
     tsc = require("gulp-tsc"),
     mocha = require("gulp-mocha"),
-    Q = require("q");
+    Q = require("q"),
+    browserify = require("browserify")
+    source = require("vinyl-source-stream");
 
 gulp.task("compile", function() {
     var defer = Q.defer();
-    gulp.src("test/{,**/}*.ts")
+    gulp.src("./test/{,**/}*.ts")
         .pipe(tsc())
-        .pipe(gulp.dest(".tmp/"))
+        .pipe(gulp.dest("./.tmp/"))
         .on("end", function() {
             defer.resolve();
         });
     return defer.promise;
 });
 
-gulp.task("test", ["compile"], function() {
-    gulp.src(".tmp/test/{,**/}*.js")
+gulp.task("build", ["compile"], function() {
+    return browserify("./.tmp/src/person.js", { standalone: "Person" })
+        .bundle()
+        .pipe(source("person.js"))
+        .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("test", ["build"], function() {
+    return gulp.src("./.tmp/test/{,**/}*.js")
         .pipe(mocha());
 });
